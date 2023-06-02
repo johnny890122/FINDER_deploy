5b03aa7cd52d31ccd75f80ca02b069c37148501f
張祥賢 <johnny890122>
2023-06-02 22:07:51 +0800

    加入finder

--- seeker_game/__init__.py
+++ seeker_game/__init__.py
@@ -5,6 +5,10 @@ import numpy as np
 
 from seeker_game.utility import get_current_graph, G_links, G_nodes, to_list, remove_node_and_neighbor, getRobustness, generate_ba_graph_with_density, node_centrality_criteria
 
+sys.path.append(os.path.dirname(__file__) + os.sep + '../')
+from FINDER import FINDER
+from tqdm import tqdm
+
 doc = """
 human seeker 單機版 
 """
@@ -46,16 +50,15 @@ def creating_session(subsession: Subsession):
     # size = subsession.session.config["size"]
     # density = subsession.session.config["density"]
     graph_config = subsession.session.config["graph_config"]
-    randint = subsession.session.config["randint"]
+    # randint = subsession.session.config["randint"]
+    randint = 0
 
     assert generating_process in ["ba_graph", "covert", "dark"]
     assert graph_config in ["size_low", "size_high", "density_low", "density_high"]
 
     # 初始化 graph
-    # randint = random.randint(0, 4)
-    # randint = 
     file_name = f"input/{generating_process}/{graph_config}_{randint}.txt"
-    initial_G = nx.read_adjlist(file_name)
+    initial_G = nx.read_gml(file_name)
     initial_G = nx.relabel_nodes(initial_G, lambda x: int(x) + 2)
 
     # hist = np.loadtxt(f"input/{generating_process}/finder_hist/{graph_config}_{randint}.txt", delimiter=",").tolist()
@@ -127,6 +130,26 @@ class Seeker_dismantle(Page):
         edge_remain = len(G.edges())
 
         player.num_node_removed = player.num_node - player.node_remain
+
+        dqn = FINDER()
+        model_file = './models/Model_barabasi_albert/nrange_150_250_iter_103800.ckpt'
+
+        file_path = './results'
+
+        if not os.path.exists('./results/'):
+            os.mkdir('./results/')
+        # if not os.path.exists('../results/FINDER_ND/synthetic'):
+            # os.mkdir('../results/FINDER_ND/synthetic')
+        
+        # for file in [file for file in os.listdir("input/ba_graph/") if file.endswith('.txt')]:
+        val, sol = dqn.Evaluate(f"input/ba_graph/density_low_0.txt", model_file)
+            # with open(f"results/{file}", 'w') as fout:
+            
+                # for i, s in enumerate(sol):
+                #     fout.write(f'{i}, {s}\n')
+                # fout.flush()
+        print(val, sol)
+        print("done")
 
 # Seeker 確認該回合的破壞成果
 class Seeker_confirm(Page):
