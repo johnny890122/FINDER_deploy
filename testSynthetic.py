d6e5771f81ea852b937292b31401abc376741412
johnny890122 <j087061439@gmail.com>
2023-06-28 12:40:57 +0800

    ba hxa

--- testSynthetic.py
+++ testSynthetic.py
@@ -9,22 +9,21 @@ def main():
     dqn = FINDER()
     data_test_path = './data/synthetic/'
     data_test_name = ['test']
-    model_file = './models/Model_dark/nrange_200_200_iter_464100.ckpt'
+    model_file = './models/Model_dark/nrange_200_200_iter_154500.ckpt'
 
     file_path = './results'
 
     if not os.path.exists('./results/'):
         os.mkdir('./results/')
-    # if not os.path.exists('../results/FINDER_ND/synthetic'):
-        # os.mkdir('../results/FINDER_ND/synthetic')
 
     for i in range(100):
-        with open(f"data/dark/finder_node_hist/g_{i}.txt", 'w') as fout:
-            val, sol = dqn.Evaluate(f"data/dark/g_{i}", model_file)
+        with open(f"data/ba/finder_node_hist/g_{i}.txt", 'w') as fout:
+            val, sol = dqn.Evaluate(f"data/ba/g_{i}", model_file)
             for i, s in enumerate(sol):
                 fout.write(f'{i}, {s}\n')
             fout.flush()
         print("done")
+        break
 
 if __name__=="__main__":
     main()
